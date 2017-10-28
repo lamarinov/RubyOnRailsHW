425a34cf8e246e8daab2b0f77df8df767b761ae5
Tsvetan Marinov <lamarinov@gmail.com>
2017-10-28 20:24:26 +0300

    Create base class hero and constructor

---
+++ heroesgame.rb
@@ -0,0 +1,11 @@
+class Hero
+  attr_accessor vitality
+
+  def initialize(name, nickname, strength)
+    @name = name
+    @nickname = nickname
+    @strength = strength
+    @vitality = 100
+  end
+
+end
